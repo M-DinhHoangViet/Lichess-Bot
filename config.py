99fd71992545275fbb0d5ed22b3890b96c2d1df7
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2023-08-17 11:09:30 +0700

    Update config.py
    Signed-off-by: Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>

--- config.py
+++ config.py
@@ -175,6 +175,6 @@ def load_config(config_path: str) -> dict:
         commit_SHA = output.decode('utf-8').strip()[:7]
         CONFIG['version'] = f'{commit_date}-{commit_SHA}'
     except (FileNotFoundError, subprocess.CalledProcessError):
-        CONFIG['version'] = 'v.2.0.2'
+        CONFIG['version'] = 'v.2.1.2'
 
     return CONFIG
