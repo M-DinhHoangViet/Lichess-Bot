8e8424192049b3921dd24024642687458ad27200
Đinh Hoàng Việt <vietblue1403@gmail.com>
2023-08-26 13:39:47 +0700

    Update update_version.py

--- .github/workflows/update_version.py
+++ .github/workflows/update_version.py
@@ -3,10 +3,10 @@ import os
 import datetime
 import yaml
 
-with open('versioning.yml') as version_file:
+with open("versioning.yml") as version_file:
     versioning_info = yaml.safe_load(version_file)
 
-current_version = versioning_info['Lichess_Bot_version']
+current_version = versioning_info["Lichess_Bot_version"]
 
 utc_datetime = datetime.datetime.utcnow()
 new_version = f"{utc_datetime.month}.{utc_datetime.day}.{utc_datetime.year}."
@@ -17,10 +17,10 @@ if current_version.startswith(new_version):
 else:
     new_version += "1"
 
-versioning_info['Lichess_Bot_version'] = new_version
+versioning_info["Lichess_Bot_version"] = new_version
 
-with open('versioning.yml', 'w') as version_file:
+with open("versioning.yml", "w") as version_file:
     yaml.dump(versioning_info, version_file, sort_keys=False)
 
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'new_version={new_version}', file=fh)
+with open(os.environ['GITHUB_OUTPUT'], "a") as fh:
+    print(f"new_version={new_version}", file=fh)
